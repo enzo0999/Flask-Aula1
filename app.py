a1ea8a6651c5af761ff275c98df62c4d4426edbd
enzo0999 <enzogaldino@colegiopaulodetarso.com.br>
2025-11-05 16:12:46 +0000

    dewjkfbhdfjw

--- app.py
+++ app.py
@@ -1,7 +1,5 @@
 from flask import Flask
-
 app = Flask(__name__) 
-
 @app.route('/')
 def index():
     return("hello")
@@ -10,8 +8,22 @@ def index():
 def info():
     return("Página info")
 
-@app.route('/info')
-def index():
-    modulo = "flask" 
-    aula = "1"
-    return "<h1>modulo:{modulo}, Aula:{aula} </h1>"
+@app.get("/info") 
+def info(): 
+   modulo = "Flask" 
+   aula = 1 
+   return f"<h1>Módulo: {modulo} — Aula {aula}<h1>"
+
+@app.get("/bemvindo/<usuario>") 
+def bemvindo(usuario):
+   return f"<h1>Bem-vindo, {usuario.capitalize()}!<h1>"
+
+from flask import Flask, redirect
+@app.get("/home") 
+def home(): 
+  return redirect("/")
+
+from flask import Flask, redirect, render_template
+@app.get("/sobre") 
+def sobre(): 
+   return render_template("sobre.html")
