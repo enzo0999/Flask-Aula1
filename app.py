955741b346d19ec028537257b0cfe7be7bcd0279
enzo0999 <enzogaldino@colegiopaulodetarso.com.br>
2025-11-05 15:33:23 +0000

    djcihdbch

--- app.py
+++ app.py
@@ -4,4 +4,14 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return "Olá, mundo !"
+    return("hello")
+
+@app.route('/info')
+def info():
+    return("Página info")
+
+@app.route('/info')
+def index():
+    modulo = "flask" 
+    aula = "1"
+    return "<h1>modulo:{modulo}, Aula:{aula} </h1>"
